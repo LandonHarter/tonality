3846e87db50f7aba09ad773ade2c4fc15de0168b
LandonHarter <landonharter@outlook.com>
2023-09-06 23:25:28 -0400

    update lessons page

--- app/lesson/[id]/page.tsx
+++ app/lesson/[id]/page.tsx
@@ -6,6 +6,8 @@ import RequireSignIn from "@/components/require/signin";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react";
+import styles from './page.module.css';
+import Image from "next/image";
 
 export default function Lesson() {
     const id = usePathname().split('/')[2];
@@ -29,7 +31,18 @@ export default function Lesson() {
 
     return (
         <RequireSignIn>
-            <h1>{lesson.title}</h1>
+            <div className={styles.container}>
+                <Image src={lesson.image} alt={lesson.title} width={1920} height={1080} className={styles.image} />
+                <div style={{
+                    marginBottom: '30vh'
+                }} />
+
+                <div className={styles.text_content}>
+                    <h1 className={styles.title}>{lesson.title}</h1>
+                    <p className={styles.description}>{lesson.description}</p>
+                </div>
+                <iframe width="560" height="315" className={styles.video} src={lesson.video.replace("watch?v=", "embed/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </RequireSignIn>
     );
 }
