d1caa8355c3087fa4d6405632267718f686a6be7
LandonHarter <landonharter@outlook.com>
2023-09-08 14:06:15 -0400

    search collections

--- app/dashboard/pages/collection.tsx
+++ app/dashboard/pages/collection.tsx
@@ -6,9 +6,34 @@ import { useEffect, useState } from 'react';
 import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from '@/backend/firebase';
 import Link from 'next/link';
+import algoliasearch from 'algoliasearch';
 
 export default function Collections() {
     const [collections, setCollections] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchClient, setSearchClient] = useState<any>(null);
+    const [searchIndex, setSearchIndex] = useState<any>(null);
+
+    async function search(query: string) {
+        if (!searchClient || !searchIndex) {
+            return;
+        }
+
+        setLoading(true);
+        const { hits } = await searchIndex.search(query);
+        const collectionsData: any[] = [];
+        hits.forEach((hit: any) => {
+            collectionsData.push({
+                id: hit.objectID,
+                ...hit
+            });
+        });
+
+        setCollections(collectionsData);
+        setLoading(false);
+    }
 
     useEffect(() => {
         (async () => {
@@ -23,6 +48,21 @@ export default function Collections() {
                 });
             });
             setCollections(collectionsData);
+
+            const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
+            const algoliaSearchKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY;
+
+            if (!algoliaAppId || !algoliaSearchKey) {
+                console.error('Algolia app ID or search key not found');
+                return;
+            }
+
+            const algoliaClient = algoliasearch(algoliaAppId, algoliaSearchKey);
+            const algoliaIndex = algoliaClient.initIndex('collections');
+            setSearchClient(algoliaClient);
+            setSearchIndex(algoliaIndex);
+
+            setLoading(false);
         })();
     }, []);
 
@@ -32,20 +72,45 @@ export default function Collections() {
                 <Image src="/images/icons/collection.png" alt='home' width={32} height={32} />
                 <h1>Collections</h1>
             </div>
+            <div className={styles.search}>
+                <input type="text" placeholder="Search for collections" className={styles.search_input} value={searchQuery} onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        search(searchQuery);
+                    }
+                }} onChange={(e) => {
+                    setSearchQuery(e.target.value);
+                }} />
+                <button className={styles.submit_search} onClick={() => {
+                    search(searchQuery);
+                }}>
+                    <Image src="/images/icons/search.png" alt='search' width={30} height={30} />
+                </button>
+            </div>
 
-            {collections.map((collection: any, index: number) => {
-                return (
-                    <Link href={`/collection/${collection.id}`} key={index} style={{ textDecoration: 'none' }}>
-                        <div className={styles.collection}>
-                            <div>
-                                <h1>{collection.title}</h1>
-                                <p>{collection.description}</p>
-                            </div>
-                            <h4>{collection.lessons.length} Lessons</h4>
-                        </div>
-                    </Link>
-                );
-            })}
+            {loading ?
+                <div className={styles.loading}>
+                    <div className={styles.loader}>
+                        <div className={styles.dot}></div>
+                        <div className={styles.dot}></div>
+                        <div className={styles.dot}></div>
+                    </div>
+                    <h1 className={styles.loading_title}>Loading...</h1>
+                </div> : <>
+                    {collections.map((collection: any, index: number) => {
+                        return (
+                            <Link href={`/collection/${collection.id}`} key={index} style={{ textDecoration: 'none' }}>
+                                <div className={styles.collection}>
+                                    <div>
+                                        <h1>{collection.title}</h1>
+                                        <p>{collection.description}</p>
+                                    </div>
+                                    <h4>{collection.lessons.length} Lessons</h4>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </>
+            }
         </>
     );
 }
